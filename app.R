a164f9fef5742b690bb92bd15805179c4ee860b5
vramdas06 <115605672+vramdas06@users.noreply.github.com>
2025-09-30 10:27:16 -0500

    add action buttons

--- app.R
+++ app.R
@@ -9,23 +9,47 @@ ui <- fluidPage(
                          value = 0),
             numericInput(inputId = "number2",
                          label = "Number 2",
-                         value = 0)
+                         value = 0),
+            actionButton(inputId = "abutton",
+                         label = "Add"),
+            actionButton(inputId = "mbutton",
+                         label = "Multiply")
         ),
         mainPanel(
             h2("The sum of the two numbers is:"),
-            textOutput("sum")
+            textOutput("sum"),
+            
+            h2("The product of the two numbers is:"),
+            textOutput("multiply")
         )
     )
 )
 
 server <- function(input, output) {
-    output$sum <- renderText({
-        x1 <- as.numeric(input$number1)
-        x2 <- as.numeric(input$number2)
-        x1 + x2
-    })
-}
+  x1 <- eventReactive(input$abutton, {
+    as.numeric(input$number1)
+  })
+  
+  x2 <- eventReactive(input$abutton, {
+    as.numeric(input$number2)
+  })
 
+    n1 <- eventReactive(input$mbutton, {
+      as.numeric(input$number1)
+    })
+    
+    n2 <- eventReactive(input$mbutton, {
+      as.numeric(input$number2)
+    })
+    
+    output$sum <- renderText({
+      x1() + x2()
+    })
+    
+    output$multiply <- renderText({
+      n1() * n2()
+      })
+    }
 shinyApp(ui = ui, server = server)
 
 
